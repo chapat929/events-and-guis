1aa5d89ab3f1fb4a0afbedb420bfcce73995f187
Chandni Patel <chandni_patel@gap.com>
2016-06-16 08:31:35 -0700

    Added Action Listener to button

--- src/main/java/com/chandni/button/SimpleGui.java
+++ src/main/java/com/chandni/button/SimpleGui.java
@@ -1,18 +1,29 @@
 package com.chandni.button;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleGui {
+public class SimpleGui implements ActionListener{
+
+    private final JButton button = new JButton();
 
     public void go() {
         final JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
 
-        final JButton button = new JButton();
         button.setText("Click Me");
         frame.getContentPane().add(button);
+        button.addActionListener(this);
 
+        //This must be after the button is added to the content pane
         frame.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I've been clicked at least once");
     }
 }
